ac132187ff8cdd4d4c4df8260479193f12878c45
Prakash Sellathurai <prakashsellathurai@gmail.com>
2026-01-28 11:25:01 +0530

    Backup Daemon: Auto commit on 20260128112500

--- experiments/biology/dna-sequencing/pattern-extraction.py
+++ experiments/biology/dna-sequencing/pattern-extraction.py
@@ -1,9 +1,47 @@
+def computerLPS(pattenrn):
+    n = len(pattenrn)
+    lps = [0]*n 
+    len_ = 0
+    i = 1
+
+    while i< n:
+        if pattenrn[i] == pattenrn[len_]:
+            len_+=1
+            lps[i] = len_
+            i+=1
+        else:
+            if len_ != 0:
+                len_ = lps[len_-1]
+            else:
+                lps[i] = 0
+                i+=1
+    return lps
+
 def stringmatch(s, pat):
-    # s  a a a a a a b
-    # pat a a a b
-    #  
+    lps = computerLPS(pat)
+    n = len(s)
+    m = len(pat) 
+
+    i = 0
+    j = 0
+    matches = []
+    while i<n:
+        if s[i] == pat[j]:
+            i+=1
+            j+=1
+            if j == m:
+                matches.append((i,i-j))
+                j = lps[j-1]
+        else:
+            if j != 0:
+                j = lps[j-1]
+            else:
+                i+=1
+    return matches
     
 
-s = "aaaaaaaab"
-pat = "aaab"
-stringmatch(s, pat)
+s = "ABC ABCDAB ABCDABCDABDE"
+pat = "ABCDABD"
+
+if __name__ =="__main__":
+    print(stringmatch(s, pat))
