919a6e0ea9c03bc50c850af11d94cadab1026d1a
prakash S <prakashsellathurai@gmail.com>
2026-04-13 15:37:55 +0000

    Backup Daemon: Auto commit on 20260413153729

--- tools/scripts/gitcommitandpush.sh
+++ tools/scripts/gitcommitandpush.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 commitMessage="Backup Daemon: Auto commit on `date +%Y%m%d%H%M%S`";
 git submodule foreach --recursive "git add ."
-git submodule foreach "echo 'Committing changes.'; git commit -a -q -m '${commitMessage}' || :"
+git submodule foreach --recursive \
+  "git diff --cached --quiet && git diff --quiet || git commit -a -q -m '${commitMessage}' || :"
+
 git add .
 git commit -m "$commitMessage"
 git push origin main --recurse-submodules=on-demand
